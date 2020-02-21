732202f6e91236a0fc5e6ebbb35f15ae219425d3
Stefan Eriksson <stefan.k.j.eriksson@gmail.com>
2020-02-21 14:17:56 +0100

    Added debug to file functionallity.

---
+++ Common/Debug.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace GoldDigger.Common
+{
+    public static class Debug
+    {
+        public static void ToFile(Exception e, bool append = false)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var name = "GoldDiggerDebug.txt";
+            var filePath = Path.Combine(path, name);
+            var file = new FileInfo(filePath);
+
+            var writer = append ? File.AppendText(file.FullName) : new StreamWriter(file.FullName);
+            using (writer)
+            {
+                writer.WriteLine(e.Message);
+                writer.WriteLine("");
+                writer.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
